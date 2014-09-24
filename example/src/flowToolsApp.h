646c140bdee56a4b1fd49aaaad9b15f559111e64
Matthias Oostrik <m.oostrik@gmail.com>
2014-09-24 17:32:52 +0200

    Example updated

--- example/src/flowToolsApp.h
+++ example/src/flowToolsApp.h
@@ -5,7 +5,7 @@
 #include "ofxFlowTools.h"
 
 #define USE_PROGRAMMABLE_GL					// Maybe there is a reason you would want to
-//#define USE_FASTER_INTERNAL_FORMATS			// 15% faster but gives errors from ofGLUtils
+//#define USE_FASTER_INTERNAL_FORMATS			// about 15% faster but gives errors from ofGLUtils
 
 using namespace flowTools;
 

c93a4c9eda417b870d0b0fb3a674d4f9c4b5ab88
james sheridan <sheridanis@gmail.com>
2015-09-23 23:02:40 +0100

    Fix example project files to include missing file Fix bug in velocity draw force where width was used instead of height

--- src/drawforces/ftDrawForce.cpp
+++ src/drawforces/ftDrawForce.cpp
@@ -79,7 +79,7 @@ namespace flowTools {
 		ofVec4f typeForce = force;
 		switch (type) {
 			case FT_VELOCITY:
-				typeForce *= ofVec4f(width, width, 0, 1);
+				typeForce *= ofVec4f(width, height, 0, 1);
 				break;
 			case FT_PRESSURE:
 				typeForce *= ofVec4f(100, 0, 0, 1);

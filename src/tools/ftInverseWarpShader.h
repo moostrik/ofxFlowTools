98e755837fad425d944f727c3b11407a7bcb5087
m.oostrik <matthias@oostrik.net>
2017-07-13 19:05:24 +0200

    glsl 150 and Scale

--- src/tools/ftInverseWarpShader.h
+++ src/tools/ftInverseWarpShader.h
@@ -27,7 +27,6 @@ namespace flowTools {
 		void glTwo() {
 			fragmentShader = GLSL120(
 									 uniform sampler2DRect tex0;
-									 
 									 uniform vec2	Scale;
 									 
 									 uniform vec2 Upper_left;
@@ -40,14 +39,13 @@ namespace flowTools {
 										 
 										 vec2 st2 = st * Scale;
 										 
+										 float sourceXUp = st2.x * (Upper_right.x - Upper_left.x) + Upper_left.x;
+										 float sourceXDown = st2.x * (Lower_right.x - Lower_left.x) + Lower_left.x;
+										 float sourceX = mix(sourceXUp, sourceXDown, st2.y);
 										 
-										 float sourceXUp = st.x * (Upper_right.x - Upper_left.x) + Upper_left.x;
-										 float sourceXDown = st.x * (Lower_right.x - Lower_left.x) + Lower_left.x;
-										 float sourceX = mix(sourceXUp, sourceXDown, st.y);
-										 
-										 float sourceYLeft = st.y * (Lower_left.y - Upper_left.y) + Upper_left.y;
-										 float sourceYRight = st.y * (Lower_right.y - Upper_right.y) + Upper_right.y;
-										 float sourceY = mix(sourceYLeft, sourceYRight, st.x);
+										 float sourceYLeft = st2.y * (Lower_left.y - Upper_left.y) + Upper_left.y;
+										 float sourceYRight = st2.y * (Lower_right.y - Upper_right.y) + Upper_right.y;
+										 float sourceY = mix(sourceYLeft, sourceYRight, st2.x);
 										 
 										 gl_FragColor = texture2DRect(tex0,vec2(sourceX * Scale.x, sourceY * Scale.y));
 									 }
@@ -59,22 +57,32 @@ namespace flowTools {
 		
 		void glThree() {
 			fragmentShader = GLSL150(
-								  uniform sampler2DRect Backbuffer;
-								  uniform sampler2DRect AddTexture;
-								  uniform float force;
-								  uniform vec2	Scale;
-								  
-								  in vec2 texCoordVarying;
-								  out vec4 fragColor;
-								  
-								  void main(){
-									  vec2 st = texCoordVarying;
-									  vec2 st2 = st * Scale;
-									  
-									  vec4 color = texture(Backbuffer, st) + texture(AddTexture, st2) * force;
-									  fragColor = color ;
-								  }
-								  );
+									 uniform sampler2DRect tex0;
+									 uniform vec2	Scale;
+									 
+									 uniform vec2 Upper_left;
+									 uniform vec2 Upper_right;
+									 uniform vec2 Lower_left;
+									 uniform vec2 Lower_right;
+									 
+									 in vec2 texCoordVarying;
+									 out vec4 fragColor;
+									 
+									 void main(){
+										 vec2 st = texCoordVarying;
+										 vec2 st2 = st * Scale;
+										 
+										 float sourceXUp = st2.x * (Upper_right.x - Upper_left.x) + Upper_left.x;
+										 float sourceXDown = st2.x * (Lower_right.x - Lower_left.x) + Lower_left.x;
+										 float sourceX = mix(sourceXUp, sourceXDown, st2.y);
+										 
+										 float sourceYLeft = st2.y * (Lower_left.y - Upper_left.y) + Upper_left.y;
+										 float sourceYRight = st2.y * (Lower_right.y - Upper_right.y) + Upper_right.y;
+										 float sourceY = mix(sourceYLeft, sourceYRight, st2.x);
+										 
+										 fragColor = texture(tex0,vec2(sourceX * inputDimentions.x, sourceY * inputDimentions.y));
+									 }
+									 );
 			
 			bInitialized *= shader.setupShaderFromSource(GL_VERTEX_SHADER, vertexShader);
 			bInitialized *= shader.setupShaderFromSource(GL_FRAGMENT_SHADER, fragmentShader);
@@ -88,7 +96,7 @@ namespace flowTools {
 			_buffer.begin();
 			shader.begin();
 			shader.setUniformTexture("tex0", _texture, 0);
-			shader.setUniform2f("Scale", _texture.getWidth(), _texture.getHeight());
+			shader.setUniform2f("Scale", _texture.getWidth() / _buffer.getWidth(), _texture.getHeight()/ _buffer.getHeight());
 			shader.setUniform2f("Upper_left",  _ul.x, _ul.y);
 			shader.setUniform2f("Upper_right", _ur.x, _ur.y);
 			shader.setUniform2f("Lower_left",  _ll.x, _ll.y);

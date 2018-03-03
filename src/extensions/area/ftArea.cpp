0f6b49d7cf943d8aba54c6e4ecd7e84b3b809970
Matthias Oostrik <m.oostrik@gmail.com>
2018-03-03 14:33:38 +0100

    refactoring

--- src/extensions/area/ftArea.cpp
+++ src/extensions/area/ftArea.cpp
@@ -9,6 +9,9 @@ namespace flowTools {
 	
 		width = _width;
 		height = _height;
+//		scaleFbo.allocate(width, height); // allocate 
+//		allocate(scaleFbo.getTexture());
+		
 		numPixels = _width * _height;
 		roi = ofRectangle(0,0,1,1);
 		
@@ -28,42 +31,16 @@ namespace flowTools {
 		}
 		
 		roiParameters.setName("ROI " + _name);
-		roiParameters.add(pRoiX.set("x", 0, 0, 1));
-		pRoiX.addListener(this, &ftArea::pRoiXListener);
-		roiParameters.add(pRoiY.set("y", 0, 0, 1));
-		pRoiY.addListener(this, &ftArea::pRoiYListener);
-		roiParameters.add(pRoiWidth.set("width", 1, 0, 1));
-		roiParameters.add(pRoiHeight.set("height", 1, 0, 1));
-		parameters.add(roiParameters);
-	}
-	
-	void ftArea::allocate(ofTexture &_tex) {
-		ofTextureData& texData = _tex.getTextureData();
-		
-		internalFormat = texData.glInternalFormat;
-		bAllocated = true;
-		
-		switch(internalFormat){
-			case GL_R32F: 		numChannels = 1; break;
-			case GL_RG32F: 		numChannels = 2; break;
-			case GL_RGB32F: 	numChannels = 3; break;
-			case GL_RGBA32F:	numChannels = 4; break;
-			default:
-				numChannels = 0;
-				bAllocated = false;
-				ofLogWarning("ftArea") << "allocate: " << "ftArea works with float textures only";
-				return;
+		pRoi.resize(4);
+		roiParameters.add(pRoi[0].set("x", 0, 0, 1));
+		roiParameters.add(pRoi[1].set("y", 0, 0, 1));
+		roiParameters.add(pRoi[2].set("width", 1, 0, 1));
+		roiParameters.add(pRoi[3].set("height", 1, 0, 1));
+		for (int i=0; i<4; i++) {
+			pRoi[i].addListener(this, &ftArea::pFloatListener);
+			pRoi[i].addListener(this, &ftArea::pRoiListener);
 		}
-		
-		scaleFbo.allocate(width, height, internalFormat);
-		ftUtil::black(scaleFbo);
-		
-		direction.clear();
-		direction.resize(numChannels, 0);
-		velocity.clear();
-		velocity.resize(numChannels, 0);
-		
-		pixels.allocate(width, height, numChannels);
+		parameters.add(roiParameters);
 	}
 	
 	void ftArea::update(ofTexture& _texture) {
@@ -111,6 +88,55 @@ namespace flowTools {
 		
 		pMeanMagnitude.set(meanMagnitude);
 		pStdevMagnitude.set(stdevMagnitude);
+	}
+	
+	void ftArea::allocate(ofTexture &_tex) {
+		ofTextureData& texData = _tex.getTextureData();
+		
+		cout << "allocate" << endl;
+		
+		internalFormat = texData.glInternalFormat;
+		bAllocated = true;
+		
+		switch(internalFormat){
+			case GL_R32F: 		numChannels = 1; break;
+			case GL_RG32F: 		numChannels = 2; break;
+			case GL_RGB32F: 	numChannels = 3; break;
+			case GL_RGBA32F:	numChannels = 4; break;
+			default:
+				numChannels = 0;
+				bAllocated = false;
+				ofLogWarning("ftArea") << "allocate: " << "ftArea works with float textures only";
+				return;
+		}
+		
+		scaleFbo.allocate(width, height, internalFormat);
+		ftUtil::black(scaleFbo);
+		
+		direction.clear();
+		direction.resize(numChannels, 0);
+		velocity.clear();
+		velocity.resize(numChannels, 0);
+		
+		pixels.allocate(width, height, numChannels);
+	}
+	
+	void ftArea::setRoi(ofRectangle _rect) {
+		float x = _rect.x;
+		float y = _rect.y;
+		float maxW = 1.0 - x;
+		float maxH = 1.0 - y;
+		float w = min(_rect.width, maxW);
+		float h = min(_rect.height, maxH);
+		
+		roi = ofRectangle(x, y, w, h);
+		
+		if (pRoi[0] != x) { pRoi[0].set(x); }
+		if (pRoi[1] != y) { pRoi[1].set(y); }
+		if (pRoi[2].getMax() != maxW) { pRoi[2].setMax(maxW); pRoi[2].set(w); }
+		if (pRoi[3].getMax() != maxH) { pRoi[3].setMax(maxH); pRoi[3].set(h); }
+		if (pRoi[2] != w) { pRoi[2].set(w); }
+		if (pRoi[3] != h) { pRoi[3].set(h); }
 	}
 	
 	void ftArea::getMeanStDev(vector<float> &_v, float &_mean, float &_stDev) {
--- src/extensions/area/ftArea.h
+++ src/extensions/area/ftArea.h
@@ -14,43 +14,37 @@ namespace flowTools {
 		void			setup(int _width = 32, int _height = 32, string _name = "");
 		void			update(ofTexture& _texture);
 		
-		void			setRoi(float _x, float _y, float _width, float _height) { pRoiX.set(_x); pRoiY.set(_y); pRoiWidth.set(_width); pRoiHeight.set(_height); roi = ofRectangle(pRoiX, pRoiY, pRoiWidth, pRoiHeight); }
-		void			setRoi(ofRectangle _rect) { setRoi(_rect.x, _rect.y, _rect.width, _rect.height); }
+//		void			setWidth(int _value)	{ bAllocated = false; width = _value; }
+//		void			setHeight(int _value)	{ bAllocated = false; height = _value; }
+
+		void			setRoi(float _x, float _y, float _width, float _height) { setRoi(ofRectangle(_x, _y, _width, _height)); }
+		void			setRoi(ofRectangle _rect);
 
 		int				getNumChannels()		{ return numChannels; }
-		
+
 		float			getDirection(int _index){ if (_index < numChannels) { return direction[_index]; } else { return 0; } }
-		float			getVelocity(int _index) 	{ if (_index < numChannels) { return velocity[_index];  } else { return 0; } }
+		float			getVelocity(int _index) { if (_index < numChannels) { return velocity[_index];  } else { return 0; } }
 		float			getAverageMagnitude()	{ return getMeanMagnitude(); }
 		float			getMeanMagnitude()		{ return meanMagnitude; }
 		float			getStDevMagnitude()		{ return stdevMagnitude; }
+//
+//		int				getWidth()				{ return width; }
+//		int				getHeight()				{ return height; }
 
 		ofRectangle		getRoi()				{ return roi; }
-		ofPoint			getCentre()				{ return ofPoint(roi.x + roi.width / 2, roi.y + roi.height / 2); }
-
+		ofPoint			getRoiCentre()			{ return ofPoint(roi.x + roi.width / 2, roi.y + roi.height / 2); }
+		
 		ofParameterGroup 	parameters;
 		ofParameterGroup 	roiParameters;
 	protected:
 		
 		void allocate(ofTexture& _tex);
 		
-		ofParameter<float>		pMeanMagnitude;
-		ofParameter<float>		pStdevMagnitude;
-		vector< ofParameter<float> > pVelocity;
-		void pFloatListener(float& _value) { _value = int(_value * 100) / 100.0; }
-		
-		ofParameter<float>		pRoiX;
-		void pRoiXListener(float& _value) { pRoiWidth.setMax(1 - _value); if (pRoiWidth.get() > pRoiWidth.getMax()) { pRoiWidth.set(pRoiWidth.getMax());} else { pRoiWidth.set(pRoiWidth.get());} } // last else for update
-		ofParameter<float>		pRoiY;
-		void pRoiYListener(float& _value) { pRoiHeight.setMax(1 - _value); if (pRoiHeight.get() > pRoiHeight.getMax()) { pRoiHeight.set(pRoiHeight.getMax());}  else { pRoiHeight.set(pRoiHeight.get());} }
-		ofParameter<float>		pRoiWidth;
-		ofParameter<float>		pRoiHeight;
-		
 		int 			width;
-		int 	height;
-		int		numPixels;
-		int 	numChannels;
-		int 	internalFormat;
+		int 			height;
+		int				numPixels;
+		int 			numChannels;
+		int 			internalFormat;
 		bool			bAllocated;
 		
 		ofFbo			scaleFbo;
@@ -65,5 +59,13 @@ namespace flowTools {
 		vector<float> 	velocity;
 		
 		void getMeanStDev(vector<float> &_v, float &_mean, float &_stDev);
+		
+		ofParameter<float>		pMeanMagnitude;
+		ofParameter<float>		pStdevMagnitude;
+		vector< ofParameter<float> > pVelocity;
+		void pFloatListener(float& _value) { _value = int(_value * 100) / 100.0; }
+		
+		vector< ofParameter<float> > pRoi;
+		void pRoiListener(float& _value) { setRoi(pRoi[0], pRoi[1], pRoi[2], pRoi[3]); }
 	};
 }

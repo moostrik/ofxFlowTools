21da8ded7eae1edea7588e0d1037fb00d4fab52a
moostrik <m.oostrik@gmail.com>
2022-02-19 13:15:58 +0100

    respacing

--- src/core/opticalflow/ftOpticalFlow.h
+++ src/core/opticalflow/ftOpticalFlow.h
@@ -9,123 +9,123 @@
 #include "ftRGB2LuminanceShader.h"
 
 namespace flowTools {
-	
-	class ftOpticalFlow : public ftFlow {
-	public:
-		void setup(int _width, int _height) {
-			allocate(_width, _height, GL_R8, _width, _height, GL_RG32F);
-			
-			bFirstFrame = true;
-			bInputSet = false;
-			
-			parameters.setName("optical flow");
-			offset.set("offset", 3, 1, 10);
-			threshold.set("threshold", 0.1, 0, 0.2);
-			strength.set("force", 3, .1, 10);			// 3 is best for normalization
-			boost.set("boost", 0.0, 0.0, .9);			//
-			doInverseX.set("inverse x", true); 			// flow velocity is inverse to fluid velocity
-			doInverseY.set("inverse y", true); 			// flow velocity is inverse to fluid velocity
-			
-			//		parameters.add(offset);
-			parameters.add(threshold);
-			//		parameters.add(strength);
-					parameters.add(boost);
-			//		parameters.add(doInverseX);
-			//		parameters.add(doInverseY);
-		};
-		
-		void update() {
-			if (bInputSet) {
-				bInputSet = false;
-				
-				ofPushStyle();
-				ofEnableBlendMode(OF_BLENDMODE_DISABLED);
-				
-				opticalFlowFbo.swap();
-				ftUtil::stretch(opticalFlowFbo.get(), inputFbo.getTexture());
-				
-				if (bFirstFrame) {
-					bFirstFrame = false;
-					opticalFlowFbo.swap();
-					ftUtil::stretch(opticalFlowFbo.get(), opticalFlowFbo.getBackTexture());
-				}
-				
-				opticalFlowShader.update(outputFbo.get(), opticalFlowFbo.getTexture(), opticalFlowFbo.getBackTexture(), offset.get(), threshold.get(),  glm::vec2(strength.get()),  1.0 - boost.get(), doInverseX.get(), doInverseY.get());
-				
-				ofPopStyle();
-			}
-		}
-		
-		void setInput(ofTexture& _tex) override {
-			ofPushStyle();
-			ofEnableBlendMode(OF_BLENDMODE_DISABLED);
-			ftUtil::zero(inputFbo.get());
-			if (_tex.getTextureData().glInternalFormat != GL_R8) {
-				RGB2LumShader.update(inputFbo.get(), _tex); }
-			else {
-				ftUtil::stretch(inputFbo.get(), _tex);
-			}
-			bInputSet = true;
-			
-			ofPopStyle();
-		}
-		
-		void addInput(ofTexture& _tex , float _strength = 1.0) override {
-			inputFbo.swap();
-			if (_tex.getTextureData().glInternalFormat != GL_R8) {
-				RGB2LumShader.update(RGB2LumFbo, _tex);
-				addMultipliedShader.update(inputFbo.get(), inputFbo.getBackTexture(), RGB2LumFbo.getTexture(), 1.0, _strength);
-			} else {
-				addMultipliedShader.update(inputFbo.get(), inputFbo.getBackTexture(), _tex, 1.0, _strength);
-			}
-			bInputSet = true;
-			//	ofLogWarning("ftOpticalFlow: addInput") << " to the optical flow input can only be set";
-		}
-		
-		void drawInput(int _x, int _y, int _w, int _h) override { inputFbo.draw(_x, _y, _w, _h); }
-		
-		void reset() override { ftFlow::reset(); bFirstFrame = true; bInputSet = false; }
-		
-		void		setStrength(float value)	{strength.set(value);}
-		void		setBoost(float value)		{boost.set(value);}
-		void		setOffset(int value)		{offset.set(value);}
-		void		setThreshold(float value)	{threshold.set(value);}
-		void		setInverseX(bool value)		{doInverseX.set(value);}
-		void		setInverseY(bool value)		{doInverseY.set(value);}
-	
-		ofTexture&	getVelocity()	{ return getOutput(); }
-		
-		float		getStrength()	{return strength.get();}
-		float		getBoost()	{return boost.get();}
-		int		getOffset()	{return offset.get();}
-		float		getThreshold()	{return threshold.get();}
-		bool		getInverseX()	{return doInverseX.get();}
-		bool		getInverseY()	{return doInverseY.get();}
-		
-	protected:
-		ofParameter<float>			threshold;
-		ofParameter<int>			offset;
-		ofParameter<float>			strength;
-		ofParameter<float>			boost;
-		ofParameter<bool>			doInverseX;
-		ofParameter<bool>			doInverseY;
-		
-		bool						bInputSet;
-		bool						bFirstFrame;
-		ftOpticalFlowShader 		opticalFlowShader;
-		ftRGB2LuminanceShader		RGB2LumShader;
-		
-		ofFbo						RGB2LumFbo;
-		ftPingPongFbo				opticalFlowFbo;
-		
-		void allocate(int _inputWidth, int _inputHeight, GLint _inputInternalFormat, int _outputWidth, int _outputHeight, GLint _outputInternalFormat) override{
-			ftFlow::allocate(_inputWidth, _inputHeight, GL_R8, _inputWidth, _inputHeight, GL_RG32F);
-			opticalFlowFbo.allocate(_inputWidth, _inputHeight, GL_R8);
-			ftUtil::zero(opticalFlowFbo);
-			RGB2LumFbo.allocate(_inputWidth, _inputHeight, GL_R8);
-			ftUtil::zero(RGB2LumFbo);
-			bFirstFrame = true;
-			bInputSet = false;
-		}
-	};
+
+class ftOpticalFlow : public ftFlow {
+public:
+  void setup(int _width, int _height) {
+    allocate(_width, _height, GL_R8, _width, _height, GL_RG32F);
+
+    bFirstFrame = true;
+    bInputSet = false;
+
+    parameters.setName("optical flow");
+    offset.set("offset", 3, 1, 10);
+    threshold.set("threshold", 0.1, 0, 0.2);
+    strength.set("force", 3, .1, 10);      // 3 is best for normalization
+    boost.set("boost", 0.0, 0.0, .9);      //
+    doInverseX.set("inverse x", true);       // flow velocity is inverse to fluid velocity
+    doInverseY.set("inverse y", true);       // flow velocity is inverse to fluid velocity
+
+    //    parameters.add(offset);
+    parameters.add(threshold);
+    //    parameters.add(strength);
+    parameters.add(boost);
+    //    parameters.add(doInverseX);
+    //    parameters.add(doInverseY);
+  };
+
+  void update() {
+    if (bInputSet) {
+      bInputSet = false;
+
+      ofPushStyle();
+      ofEnableBlendMode(OF_BLENDMODE_DISABLED);
+
+      opticalFlowFbo.swap();
+      ftUtil::stretch(opticalFlowFbo.get(), inputFbo.getTexture());
+
+      if (bFirstFrame) {
+        bFirstFrame = false;
+        opticalFlowFbo.swap();
+        ftUtil::stretch(opticalFlowFbo.get(), opticalFlowFbo.getBackTexture());
+      }
+
+      opticalFlowShader.update(outputFbo.get(), opticalFlowFbo.getTexture(), opticalFlowFbo.getBackTexture(), offset.get(), threshold.get(),  glm::vec2(strength.get()),  1.0 - boost.get(), doInverseX.get(), doInverseY.get());
+
+      ofPopStyle();
+    }
+  }
+
+  void setInput(ofTexture& _tex) override {
+    ofPushStyle();
+    ofEnableBlendMode(OF_BLENDMODE_DISABLED);
+    ftUtil::zero(inputFbo.get());
+    if (_tex.getTextureData().glInternalFormat != GL_R8) {
+      RGB2LumShader.update(inputFbo.get(), _tex); }
+    else {
+      ftUtil::stretch(inputFbo.get(), _tex);
+    }
+    bInputSet = true;
+
+    ofPopStyle();
+  }
+
+  void addInput(ofTexture& _tex , float _strength = 1.0) override {
+    inputFbo.swap();
+    if (_tex.getTextureData().glInternalFormat != GL_R8) {
+      RGB2LumShader.update(RGB2LumFbo, _tex);
+      addMultipliedShader.update(inputFbo.get(), inputFbo.getBackTexture(), RGB2LumFbo.getTexture(), 1.0, _strength);
+    } else {
+      addMultipliedShader.update(inputFbo.get(), inputFbo.getBackTexture(), _tex, 1.0, _strength);
+    }
+    bInputSet = true;
+    //  ofLogWarning("ftOpticalFlow: addInput") << " to the optical flow input can only be set";
+  }
+
+  void drawInput(int _x, int _y, int _w, int _h) override { inputFbo.draw(_x, _y, _w, _h); }
+
+  void reset() override { ftFlow::reset(); bFirstFrame = true; bInputSet = false; }
+
+  void  setStrength(float value)  {strength.set(value);}
+  void  setBoost(float value)     {boost.set(value);}
+  void  setOffset(int value)      {offset.set(value);}
+  void  setThreshold(float value) {threshold.set(value);}
+  void  setInverseX(bool value)   {doInverseX.set(value);}
+  void  setInverseY(bool value)   {doInverseY.set(value);}
+
+  ofTexture&  getVelocity()  { return getOutput(); }
+
+  float getStrength()   {return strength.get();}
+  float getBoost()      {return boost.get();}
+  int   getOffset()     {return offset.get();}
+  float getThreshold()  {return threshold.get();}
+  bool  getInverseX()   {return doInverseX.get();}
+  bool  getInverseY()   {return doInverseY.get();}
+
+protected:
+  ofParameter<float>  threshold;
+  ofParameter<int>    offset;
+  ofParameter<float>  strength;
+  ofParameter<float>  boost;
+  ofParameter<bool>   doInverseX;
+  ofParameter<bool>   doInverseY;
+
+  bool                  bInputSet;
+  bool                  bFirstFrame;
+  ftOpticalFlowShader   opticalFlowShader;
+  ftRGB2LuminanceShader RGB2LumShader;
+
+  ofFbo         RGB2LumFbo;
+  ftPingPongFbo opticalFlowFbo;
+
+  void allocate(int _inputWidth, int _inputHeight, GLint _inputInternalFormat, int _outputWidth, int _outputHeight, GLint _outputInternalFormat) override{
+    ftFlow::allocate(_inputWidth, _inputHeight, GL_R8, _inputWidth, _inputHeight, GL_RG32F);
+    opticalFlowFbo.allocate(_inputWidth, _inputHeight, GL_R8);
+    ftUtil::zero(opticalFlowFbo);
+    RGB2LumFbo.allocate(_inputWidth, _inputHeight, GL_R8);
+    ftUtil::zero(RGB2LumFbo);
+    bFirstFrame = true;
+    bInputSet = false;
+  }
+};
 }
--- src/core/opticalflow/ftOpticalFlowShader.h
+++ src/core/opticalflow/ftOpticalFlowShader.h
@@ -5,139 +5,139 @@
 #include "ftShader.h"
 
 namespace flowTools {
-	
-	class ftOpticalFlowShader : public ftShader {
-	public:
-		ftOpticalFlowShader() {
-			bInitialized = 1;
-			if (ofIsGLProgrammableRenderer()) { glFour(); } else { glTwo(); }
-			string shaderName = "ftOpticalFlowShader";
-			if (bInitialized) { ofLogVerbose(shaderName + " initialized"); }
-			else { ofLogWarning(shaderName + " failed to initialize"); }
-		}
-		
-	protected:
-		void glTwo() {
-			fragmentShader = GLSL120(
-									 uniform sampler2DRect	tex0;
-									 uniform sampler2DRect	tex1;
-									 uniform float			offset;
-									 uniform float			threshold;
-									 uniform vec2			force;
-									 uniform float			power;
-									 
-									 void main(){
-										 vec2 st = gl_TexCoord[0].st;
-										 vec2 off_x = vec2(offset, 0.0);
-										 vec2 off_y = vec2(0.0, offset);
-										 
-										 //get the difference
-										 float scr_dif = texture2DRect(tex0, st).x - texture2DRect(tex1, st).x;
-										 
-										 //calculate the gradient
-										 float gradx; float grady; float gradmag;
-										 gradx =  texture2DRect(tex1, st + off_x).x - texture2DRect(tex1, st - off_x).x;
-										 gradx += texture2DRect(tex0, st + off_x).x - texture2DRect(tex0, st - off_x).x;
-										 grady =  texture2DRect(tex1, st + off_y).x - texture2DRect(tex1, st - off_y).x;
-										 grady += texture2DRect(tex0, st + off_y).x - texture2DRect(tex0, st - off_y).x;
-										 gradmag = sqrt((gradx*gradx)+(grady*grady)+TINY);
-										 
-										 vec2 flow;
-										 flow.x = scr_dif*(gradx/gradmag);
-										 flow.y = scr_dif*(grady/gradmag);
-										 
-										 // apply force (to normalize)
-										 flow *= force;
-										 
-										 // apply treshold and clamp
-										 float magnitude = length(flow);
-										 magnitude = max(magnitude, threshold);
-										 magnitude -= threshold;
-										 magnitude /= (1-threshold);
-										 magnitude = pow(magnitude, power);
-										 flow += TINY; // flow length cannot be 0 for normalization to work on windows
-										 flow = normalize(flow) * vec2(min(magnitude, 1));
-										 
-										 // set color
-										 gl_FragColor = vec4(flow, 0.0, 1.0);
-									 }
-									 );
-			
-			bInitialized *= setupShaderFromSource(GL_FRAGMENT_SHADER, fragmentShader);
-			bInitialized *= linkProgram();
-		}
-		
-		void glFour() {
-			fragmentShader = GLSL410(
-									 uniform sampler2DRect	tex0;
-									 uniform sampler2DRect	tex1;
-									 uniform float			offset;
-									 uniform float			threshold;
-									 uniform vec2			force;
-									 uniform float			power;
-									 
-									 in vec2 texCoordVarying;
-									 out vec4 fragColor;
-									 
-									 void main()
-									 {
-										 vec2 st = texCoordVarying;
-										 vec2 off_x = vec2(offset, 0.0);
-										 vec2 off_y = vec2(0.0, offset);
-										 
-										 //get the difference
-										 float scr_dif = texture(tex0, st).x - texture(tex1, st).x;
-										 
-										 //calculate the gradient
-										 float gradx; float grady; float gradmag; float lambda = 0.01;
-										 gradx =  texture(tex1, st + off_x).x - texture(tex1, st - off_x).x;
-										 gradx += texture(tex0, st + off_x).x - texture(tex0, st - off_x).x;
-										 grady =  texture(tex1, st + off_y).x - texture(tex1, st - off_y).x;
-										 grady += texture(tex0, st + off_y).x - texture(tex0, st - off_y).x;
-										 gradmag = sqrt((gradx*gradx)+(grady*grady)+TINY);
-										 
-										 vec2 flow;
-										 flow.x = scr_dif*(gradx/gradmag);
-										 flow.y = scr_dif*(grady/gradmag);
-										 
-										 // apply force (to normalize)
-										 flow *= force;
-										 
-										 // apply treshold and clamp
-										 float magnitude = length(flow);
-										 magnitude = max(magnitude, threshold);
-										 magnitude -= threshold;
-										 magnitude /= (1-threshold);
-										 magnitude = pow(magnitude, power);
-										 flow += TINY; // flow length cannot be 0 for normalization to work on windows
-										 flow = normalize(flow) * vec2(min(max(magnitude, 0), 1));
 
-										 // set color
-										 fragColor = vec4(flow, 0.0, 1.0);
-									 }
-									 );
-			
-			bInitialized *= setupShaderFromSource(GL_VERTEX_SHADER, vertexShader);
-			bInitialized *= setupShaderFromSource(GL_FRAGMENT_SHADER, fragmentShader);
-			bInitialized *= bindDefaults();
-			bInitialized *= linkProgram();
-		}
-		
-	public:
-		void update(ofFbo& _fbo, ofTexture& _currTex, ofTexture& _prevTex, float _offset = 3.0, float _threshold = 0.04, glm::vec2 _force = glm::vec2(1.0, 1.0), float _power = 1.0, bool _inverseX = 0, bool _inverseY = 0){
-			
-			_fbo.begin();
-			begin();
-			setUniformTexture("tex0", _currTex, 0);
-			setUniformTexture("tex1", _prevTex, 1);
-			setUniform1f("offset", _offset);
-			setUniform1f("threshold", _threshold);
-			setUniform2f("force", _force * glm::vec2(_inverseX? -1 : 1, _inverseY? -1 : 1));
-			setUniform1f("power", _power);
-			renderFrame(_fbo.getWidth(), _fbo.getHeight());
-			end();
-			_fbo.end();
-		}
-	};
+class ftOpticalFlowShader : public ftShader {
+public:
+  ftOpticalFlowShader() {
+    bInitialized = 1;
+    if (ofIsGLProgrammableRenderer()) { glFour(); } else { glTwo(); }
+    string shaderName = "ftOpticalFlowShader";
+    if (bInitialized) { ofLogVerbose(shaderName + " initialized"); }
+    else { ofLogWarning(shaderName + " failed to initialize"); }
+  }
+
+protected:
+  void glTwo() {
+    fragmentShader = GLSL120(
+    uniform sampler2DRect	tex0;
+    uniform sampler2DRect	tex1;
+    uniform float			offset;
+    uniform float			threshold;
+    uniform vec2			force;
+    uniform float			power;
+
+    void main(){
+      vec2 st = gl_TexCoord[0].st;
+      vec2 off_x = vec2(offset, 0.0);
+      vec2 off_y = vec2(0.0, offset);
+
+      //get the difference
+      float scr_dif = texture2DRect(tex0, st).x - texture2DRect(tex1, st).x;
+
+      //calculate the gradient
+      float gradx; float grady; float gradmag;
+      gradx =  texture2DRect(tex1, st + off_x).x - texture2DRect(tex1, st - off_x).x;
+      gradx += texture2DRect(tex0, st + off_x).x - texture2DRect(tex0, st - off_x).x;
+      grady =  texture2DRect(tex1, st + off_y).x - texture2DRect(tex1, st - off_y).x;
+      grady += texture2DRect(tex0, st + off_y).x - texture2DRect(tex0, st - off_y).x;
+      gradmag = sqrt((gradx*gradx)+(grady*grady)+TINY);
+
+      vec2 flow;
+      flow.x = scr_dif*(gradx/gradmag);
+      flow.y = scr_dif*(grady/gradmag);
+
+      // apply force (to normalize)
+      flow *= force;
+
+      // apply treshold and clamp
+      float magnitude = length(flow);
+      magnitude = max(magnitude, threshold);
+      magnitude -= threshold;
+      magnitude /= (1-threshold);
+      magnitude = pow(magnitude, power);
+      flow += TINY; // flow length cannot be 0 for normalization to work on windows
+      flow = normalize(flow) * vec2(min(magnitude, 1));
+
+      // set color
+      gl_FragColor = vec4(flow, 0.0, 1.0);
+    }
+    );
+
+    bInitialized *= setupShaderFromSource(GL_FRAGMENT_SHADER, fragmentShader);
+    bInitialized *= linkProgram();
+  }
+
+  void glFour() {
+    fragmentShader = GLSL410(
+    uniform sampler2DRect	tex0;
+    uniform sampler2DRect	tex1;
+    uniform float			offset;
+    uniform float			threshold;
+    uniform vec2			force;
+    uniform float			power;
+
+    in vec2 texCoordVarying;
+    out vec4 fragColor;
+
+    void main()
+    {
+      vec2 st = texCoordVarying;
+      vec2 off_x = vec2(offset, 0.0);
+      vec2 off_y = vec2(0.0, offset);
+
+      //get the difference
+      float scr_dif = texture(tex0, st).x - texture(tex1, st).x;
+
+      //calculate the gradient
+      float gradx; float grady; float gradmag; float lambda = 0.01;
+      gradx =  texture(tex1, st + off_x).x - texture(tex1, st - off_x).x;
+      gradx += texture(tex0, st + off_x).x - texture(tex0, st - off_x).x;
+      grady =  texture(tex1, st + off_y).x - texture(tex1, st - off_y).x;
+      grady += texture(tex0, st + off_y).x - texture(tex0, st - off_y).x;
+      gradmag = sqrt((gradx*gradx)+(grady*grady)+TINY);
+
+      vec2 flow;
+      flow.x = scr_dif*(gradx/gradmag);
+      flow.y = scr_dif*(grady/gradmag);
+
+      // apply force (to normalize)
+      flow *= force;
+
+      // apply treshold and clamp
+      float magnitude = length(flow);
+      magnitude = max(magnitude, threshold);
+      magnitude -= threshold;
+      magnitude /= (1-threshold);
+      magnitude = pow(magnitude, power);
+      flow += TINY; // flow length cannot be 0 for normalization to work on windows
+      flow = normalize(flow) * vec2(min(max(magnitude, 0), 1));
+
+      // set color
+      fragColor = vec4(flow, 0.0, 1.0);
+    }
+    );
+
+    bInitialized *= setupShaderFromSource(GL_VERTEX_SHADER, vertexShader);
+    bInitialized *= setupShaderFromSource(GL_FRAGMENT_SHADER, fragmentShader);
+    bInitialized *= bindDefaults();
+    bInitialized *= linkProgram();
+  }
+
+public:
+  void update(ofFbo& _fbo, ofTexture& _currTex, ofTexture& _prevTex, float _offset = 3.0, float _threshold = 0.04, glm::vec2 _force = glm::vec2(1.0, 1.0), float _power = 1.0, bool _inverseX = 0, bool _inverseY = 0){
+
+    _fbo.begin();
+    begin();
+    setUniformTexture("tex0", _currTex, 0);
+    setUniformTexture("tex1", _prevTex, 1);
+    setUniform1f("offset", _offset);
+    setUniform1f("threshold", _threshold);
+    setUniform2f("force", _force * glm::vec2(_inverseX? -1 : 1, _inverseY? -1 : 1));
+    setUniform1f("power", _power);
+    renderFrame(_fbo.getWidth(), _fbo.getHeight());
+    end();
+    _fbo.end();
+  }
+};
 }
 

9c4e11f58d3a533aaf5b322c9c15ad148c0e1c4e
Matthias Oostrik <m.oostrik@gmail.com>
2022-02-22 14:26:17 +0100

    respacing

--- src/core/ftShader.h
+++ src/core/ftShader.h
@@ -40,8 +40,8 @@ protected:
 
   void initGlFour() {
     vertexShader = GLSL410(
-          uniform mat4 modelViewProjectionMatrix;
-        uniform mat4 textureMatrix;
+    uniform mat4 modelViewProjectionMatrix;
+    uniform mat4 textureMatrix;
 
     in vec4  position;
     in vec2  texcoord;
